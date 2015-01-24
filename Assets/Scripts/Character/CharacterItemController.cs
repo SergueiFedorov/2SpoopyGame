353aed5eb602d08066317f60f0e11cb1ec6f5bfb
Serguei Fedorov <sergueifedorov@loop-depaulsecure-253-53.depaulsecure-student.depaul.edu>
2015-01-24 17:47:47 -0600

    Resetting lost work

--- Assets/Scripts/Character/CharacterItemController.cs
+++ Assets/Scripts/Character/CharacterItemController.cs
@@ -5,11 +5,25 @@ using System.Collections.Generic;
 public class CharacterItemController : MonoBehaviour {
 
 	public List<ItemTypes> items = new List<ItemTypes>();
+
+	public List<UnityEngine.UI.Image> invimg = new List<UnityEngine.UI.Image>();
+
+	public Sprite foodImg;
+	public Sprite medImg;
+	public Sprite waterImg;
+	public Sprite toolImg;
+	
 	DistanceToVictims distancesToVictims;
 	
 	// Use this for initialization
 	void Start () {
 		distancesToVictims = this.GetComponent<DistanceToVictims> ();
+
+		foreach (UnityEngine.UI.Image i in invimg) {
+			i.enabled = false;
+		}
+
+
 	}
 	
 	// Update is called once per frame
@@ -24,8 +38,44 @@ public class CharacterItemController : MonoBehaviour {
 				if (victim.CanTrade(items[0]))
 				{
 					ItemTypes returnedItem = victim.DoTrade(items[0]);
+					Debug.Log(returnedItem);
 					items.RemoveAt(0);
 					items.Add(returnedItem);
+					int invNum = items.Count - 1;
+					Mathf.Clamp(invNum, 0,3);
+
+					switch (returnedItem) 
+					{
+					case ItemTypes.None:
+					{
+						break;
+					}
+					case ItemTypes.Food:
+					{
+						invimg[invNum].enabled = true;
+						invimg[invNum].sprite = foodImg;
+						break;
+					}
+					case ItemTypes.Tools:
+					{
+						invimg[invNum].enabled = true;
+						invimg[invNum].sprite = toolImg;
+						break;
+					}
+					case ItemTypes.Water:
+					{
+						invimg[invNum].enabled = true;
+						invimg[invNum].sprite = waterImg;
+						break;
+					}
+					case ItemTypes.Medicine:
+					{
+						invimg[invNum].enabled = true;
+						invimg[invNum].sprite = medImg;
+						break;
+					}
+					}
+				
 				}
 		
 			}
