11f4860c6e6d4599e425275ddebfd55aba71e7af
Serguei Fedorov <sergueifedorov@loop-depaulsecure-253-53.depaulsecure-student.depaul.edu>
2015-01-24 14:36:10 -0600

    Movement fix. Health bar fix, the health bar now scales against it's original size

--- Assets/Scripts/Victims/healthbar.cs
+++ Assets/Scripts/Victims/healthbar.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class healthbar : MonoBehaviour {
 
-	public float maxhealh = 3;
-	public float curhealth = 3;
+	public float maxhealh = 3.0f;
+	public float curhealth = 3.0f;
 	public GameObject thehealthbar;
+
+	private Vector3 startScale;
 
 	
 	// Use this for initialization
 	void Start () {
-	
+		startScale = this.transform.localScale;
 	}
 
 	// Update is called once per frame
@@ -22,8 +24,9 @@ public class healthbar : MonoBehaviour {
 	public void TheCurrentHealth(int adj)
 	{
 		curhealth += adj;
-		thehealthbar.transform.localScale = new Vector3((curhealth/maxhealh)*9,2,0);
+		this.transform.localScale = (new Vector3((curhealth / maxhealh) * startScale.x, startScale.y,0));
 
+		Debug.Log (this.transform.localScale);
 
 		if (curhealth < 0) {
 			curhealth = 0;
