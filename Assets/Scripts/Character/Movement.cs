5b47610e3d07da674e2e250464ace41c5738718c
Serguei Fedorov <sergueifedorov@loop-depaulsecure-253-53.depaulsecure-student.depaul.edu>
2015-01-24 10:55:00 -0600

    Added the platform code

---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	Vector2 raycastDirection = new Vector2(0.0f, -1.0f);
+
+	float jumpForce = 200.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+
+		RaycastHit2D raycastHit = default(RaycastHit2D);
+		raycastHit = Physics2D.Raycast ((Vector2)this.transform.position - (Vector2.up * this.collider2D.bounds.size.y), raycastDirection, 0.1f);
+		if (raycastHit.collider != null) 
+		{
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				this.rigidbody2D.AddForce(Vector2.up * jumpForce); 
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow) && rigidbody2D.velocity.x < 10.0f) 
+		{
+			rigidbody2D.AddForce(new Vector2(10, 0));
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow) && rigidbody2D.velocity.x > -10.0f) 
+		{
+			rigidbody2D.AddForce(new Vector2(-10, 0));
+		}
+
+		Physics2D.IgnoreLayerCollision(this.gameObject.layer, LayerMask.NameToLayer("platform"), rigidbody2D.velocity.y > 0.0f);
+
+	}
+
+	void OnCollisionExit2D(Collision2D collision) 
+	{
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+
+	}
+}
