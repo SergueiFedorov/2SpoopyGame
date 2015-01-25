e04f5da6d4ff1f158acda81ba192425935916b0a
justnoodles <ho.yvonne@live.com>
2015-01-24 20:37:44 -0600

    adding camera movement

---
+++ Assets/Scripts/Character/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+ 
+	public Transform Player;
+	public float CamSpeed = 3.0f;
+
+
+	void Start ()
+	{
+
+	}
+	void Update()
+	{
+		transform.position = new Vector3(0,0,-10) + (Vector3)Vector2.Lerp (transform.position, Player.position, CamSpeed * Time.fixedDeltaTime);
+
+	}
+
+}
